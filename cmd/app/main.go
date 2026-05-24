4ecc679aa9d60f5ab36c96984abbd7933a9fddff
Danila <danila@x.com>
2026-05-24 03:18:07 +0300

    fix codes, remove errors and add termination

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -2,8 +2,11 @@ package main
 
 import (
 	"github.com/DanilAiro/kitty-app-back/internal/controllers"
+	"github.com/DanilAiro/kitty-app-back/internal/utils"
 )
 
 func main() {
 	controllers.Router()
+
+	utils.HandleTermination()
 }
